190989905a4b31b17f70ef93c520be69b90f07cc
rowancape <niklasmk.pihlajaniemi@gmail.com>
2024-10-04 23:09:53 +0300

    Endless stream of dog and cat images at your fingertips

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  Image,
+  Dimensions,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [cats, setCats] = useState(true);
+
+  const screenWidth = Dimensions.get("window").width;
+
+  const fetchImage = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        cats
+          ? "https://api.thecatapi.com/v1/images/search"
+          : "https://api.thedogapi.com/v1/images/search"
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const json = await response.json();
+      setImageUrl(json[0].url);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, [cats]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {loading ? (
+        <>
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text>Image loading</Text>
+        </>
+      ) : (
+        <>
+          <Button
+            title={cats ? "Want dogs instead?" : "Want cats instead?"}
+            onPress={() => {
+              setCats(!cats);
+              fetchImage()
+            }}
+          />
+          <Image
+            source={{ uri: imageUrl }}
+            style={{
+              width: screenWidth - 20,
+              height: screenWidth,
+              marginBottom: 10,
+              marginTop: 10,
+            }}
+          />
+          <Button title="NEXT IMAGE" onPress={fetchImage} />
+        </>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
